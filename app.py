264fa086575d98bc88c9042e08ff339f5090db3f
theodoreJordan <theodorejordan00@gmail.com>
2023-02-01 23:25:20 +0100

    Gradient Descent working !!

--- app.py
+++ app.py
@@ -1,79 +1,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+
+def normalize(data):
+    mean = np.mean(data)
+    std = np.std(data)
+    return (data - mean) / std
 
 def mean_squared_error(y_true, y_predicted):
     mse = np.sum((y_true-y_predicted)**2) / len(y_true)
     return mse
 
-def gradient_descent(x, y, iterations = 500, threshold = 0.001, rate = 0.01):
-    cur_weight = 0.1
-    cur_bias = 0.1
+def update_t_b(X, Y, Y_hat, theta_0, b_0, rate):
+    n = float(len(Y))
+    
+    derivative_t = (2 / n) * np.dot((Y_hat - Y), X)
+    derivative_b = (2 / n) * np.sum(Y_hat - Y)
+    
+    theta_1 = theta_0 - rate * derivative_t
+    b_1 = b_0 - rate * derivative_b
+    
+    return theta_1, b_1
+
+def gradient_descent(X, Y, rate, num_iterations, threshold):
+    theta, b = random.random(), random.random()
+    iter = 0
     prev_cost = None
     
-    n = float(len(x))
-    costs = []
-    weights = []
+    iterations = []
     
-    for i in range(iterations):
-        y_pred = (cur_weight * x) + cur_bias;
+    for i in range(num_iterations):
+        pred = np.dot(X, theta) + b
+        cost = mean_squared_error(Y, pred)
         
-        cur_cost = mean_squared_error(y, y_pred)
-        
-        if prev_cost and abs(prev_cost - cur_cost) <= threshold:
+        if prev_cost and abs(prev_cost - cost) <= threshold:
             break
         
-        prev_cost = cur_cost
+        prev_cost = cost
         
-        costs.append(cur_cost)
-        weights.append(cur_weight)
+        theta, b = update_t_b(X, Y, pred, theta, b, rate)
         
-        # gradients
-        weight_derivative = - (2 / n) * sum(x * (y - y_pred))
-        bias_derivative = - (2 / n) * sum(y - y_pred)
+        if(i%10==0):
+            iterations.append([i, cost])
         
-        # update weight and bias
-        cur_weight = cur_weight - (rate * weight_derivative)
-        cur_bias = cur_bias - (rate * bias_derivative)
+        iter += 1
         
-        print(f"Iteration {i+1}: Cost {cur_cost}, Weight \
-        {cur_weight}, Bias {cur_bias}")
-    
-    plt.figure(figsize = (8,6))
-    plt.plot(weights, costs)
-    plt.scatter(weights, costs, marker='o', color='red')
-    plt.title("Cost vs Weights")
-    plt.ylabel("Cost")
-    plt.xlabel("Weight")
+    iter = [x for x,_ in iterations]
+    costs = [y for _,y in iterations]
+    plt.plot(iter, costs)
+    plt.ylabel("Cost or MSE")
+    plt.xlabel("Iterations")
     plt.show()
-    
-    return cur_weight, cur_bias
+    return b, theta
 
 def main():
     X = np.array([32.50234527, 53.42680403, 61.53035803, 47.47563963, 59.81320787,
            55.14218841, 52.21179669, 39.29956669, 48.10504169, 52.55001444,
            45.41973014, 54.35163488, 44.1640495 , 58.16847072, 56.72720806,
            48.95588857, 44.68719623, 60.29732685, 45.61864377, 38.81681754])
+    
     Y = np.array([31.70700585, 68.77759598, 62.5623823 , 71.54663223, 87.23092513,
            78.21151827, 79.64197305, 59.17148932, 75.3312423 , 71.30087989,
            55.16567715, 82.47884676, 62.00892325, 75.39287043, 81.43619216,
            60.72360244, 82.89250373, 97.37989686, 48.84715332, 56.87721319])
     
-    rand = np.random.RandomState(19)
-    w_init = rand.uniform(-10,10,2)
+    X = normalize(X)
+    Y = normalize(Y)
     
-    est_weight, est_bias = gradient_descent(X, Y, iterations=500, threshold=0.000001, rate=0.0001)
-    print(f"Estimated Weight: {est_weight}\nEstimated Bias: {est_bias}")
+    b, theta = gradient_descent(X, Y, rate=0.01, num_iterations=2000, threshold = 0.00000000001)
     
-    Y_pred = est_weight*X + est_bias
- 
-    # Plotting the regression line
-    plt.figure(figsize = (8,6))
-    plt.scatter(X, Y, marker='o', color='red')
-    plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='blue',markerfacecolor='red',
-             markersize=10,linestyle='dashed')
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.show()
+    Y_pred = theta * X + b
     
 if __name__=="__main__":
     main()
