247c46bd563cfb0ba0720adb7380c6cbb94f6b18
theodoreJordan <theodorejordan00@gmail.com>
2023-02-02 23:01:46 +0100

    LDA debut

---
+++ GradientDescent.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+class GradientDescent:
+    def mean_squared_error(self, y_true, y_predicted):
+        mse = np.sum((y_true-y_predicted)**2) / len(y_true)
+        return mse
+    
+    def update_t_b(self, X, Y, Y_hat, theta_0, b_0, rate):
+        n = float(len(Y))
+    
+        derivative_t = (2 / n) * np.dot((Y_hat - Y), X)
+        derivative_b = (2 / n) * np.sum(Y_hat - Y)
+        
+        theta_1 = theta_0 - rate * derivative_t
+        b_1 = b_0 - rate * derivative_b
+        
+        return theta_1, b_1
+    
+    def gradient_descent(self, X, Y, rate, num_iterations, threshold, show):
+        theta, b = random.random(), random.random()
+        iter = 0
+        prev_cost = None
+        
+        iterations = []
+        
+        for i in range(num_iterations):
+            pred = np.dot(X, theta) + b
+            cost = self.mean_squared_error(Y, pred)
+            
+            if prev_cost and abs(prev_cost - cost) <= threshold:
+                break
+            
+            prev_cost = cost
+            
+            theta, b = self.update_t_b(X, Y, pred, theta, b, rate)
+            
+            if(i%10==0):
+                iterations.append([i, cost])
+            
+            iter += 1
+            
+        if(show):
+            iter = [x for x,_ in iterations]
+            costs = [y for _,y in iterations]
+            plt.plot(iter, costs)
+            plt.ylabel("Cost or MSE")
+            plt.xlabel("Iterations")
+            plt.show()
+            
+        return b, theta
--- app.py
+++ app.py
@@ -1,57 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
+
+from numpy.random import MT19937, RandomState, SeedSequence
+from GradientDescent import GradientDescent
+from LDA import LDA
 
 def normalize(data):
     mean = np.mean(data)
     std = np.std(data)
     return (data - mean) / std
 
-def mean_squared_error(y_true, y_predicted):
-    mse = np.sum((y_true-y_predicted)**2) / len(y_true)
-    return mse
+def rotate_cov(A, theta):
+    R = np.array([[np.cos(theta), -np.sin(theta)],
+                  [np.sin(theta), np.cos(theta)]])
+    return R @ A @ R.T
 
-def update_t_b(X, Y, Y_hat, theta_0, b_0, rate):
-    n = float(len(Y))
-    
-    derivative_t = (2 / n) * np.dot((Y_hat - Y), X)
-    derivative_b = (2 / n) * np.sum(Y_hat - Y)
-    
-    theta_1 = theta_0 - rate * derivative_t
-    b_1 = b_0 - rate * derivative_b
-    
-    return theta_1, b_1
+def make_gaussian_blobs(means, covs, Ns = 100, seed = 42):
+    ''' Create different blobs, each blob being a gaussian 
+        from given mean and covariance
+    '''
+    rs = RandomState(MT19937(SeedSequence(seed)))
+    M = min(len(means), len(covs))
 
-def gradient_descent(X, Y, rate, num_iterations, threshold):
-    theta, b = random.random(), random.random()
-    iter = 0
-    prev_cost = None
-    
-    iterations = []
-    
-    for i in range(num_iterations):
-        pred = np.dot(X, theta) + b
-        cost = mean_squared_error(Y, pred)
-        
-        if prev_cost and abs(prev_cost - cost) <= threshold:
-            break
-        
-        prev_cost = cost
-        
-        theta, b = update_t_b(X, Y, pred, theta, b, rate)
-        
-        if(i%10==0):
-            iterations.append([i, cost])
-        
-        iter += 1
-        
-    iter = [x for x,_ in iterations]
-    costs = [y for _,y in iterations]
-    plt.plot(iter, costs)
-    plt.ylabel("Cost or MSE")
-    plt.xlabel("Iterations")
-    plt.show()
-    return b, theta
+    if not isinstance(Ns, list):
+        aux = [Ns for _ in range(M)]
+        Ns = aux
+    X = np.concatenate([rs.multivariate_normal(m,
+                                               cov = S,
+                                               size = N)
+                        for m, S, N in zip(means, covs, Ns)
+                        ],
+                       axis=0)
+    Y = []
+
+    for i, N in enumerate(Ns):
+        Y += [i]*N
+
+    Y = np.array(Y).ravel()
+
+    return X, Y
 
 def main():
     X = np.array([32.50234527, 53.42680403, 61.53035803, 47.47563963, 59.81320787,
@@ -67,9 +54,43 @@ def main():
     X = normalize(X)
     Y = normalize(Y)
     
-    b, theta = gradient_descent(X, Y, rate=0.01, num_iterations=2000, threshold = 0.00000000001)
+    gd = GradientDescent()
+    b, theta = gd.gradient_descent(X, Y, rate=0.01, num_iterations=2000, threshold = 0.00000000001, show = False)
     
-    Y_pred = theta * X + b
+    # Y_pred = theta * X + b
+    # print(theta, " ", b)
+    
+    np.random.seed(1)
+
+    # Generate 150 samples of 4-dimensional data
+    data = np.random.randn(150, 2)
+    
+    # print(data)
+    
+    means = np.array([[0, 2], [1, -1]])
+    covs = [rotate_cov(np.array([[1, 0.9],
+                                [0.9, 1]]),np.pi/180*30),
+            rotate_cov(np.array([[1, 0.2],
+                                [0.2, 1]]),np.pi/180*0)
+    ]
+    X, Y = make_gaussian_blobs(means, covs, Ns = [100, 100], seed = 42)
+
+    fig, ax = plt.subplots(1,1, figsize = (7,7))
+    ax.scatter(X[:,0], X[:,1], c = Y)
+    
+    # print(X[:100])
+    # print(len(X[:100, 0]))
+    # print(X[:,1][50:])
+    print(Y)
+    testPoint = [4, -2]
+    
+    lda = LDA(X[:100], X[100:], len(X))
+    lda.generate_parameters()
+    cl = lda.getClass(testPoint)
+    print(cl)
+    
+    ax.scatter(testPoint[0], testPoint[1], color = 'yellow' if cl == 1 else 'purple')
+    plt.show()
     
 if __name__=="__main__":
     main()
